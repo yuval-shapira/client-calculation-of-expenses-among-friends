4ff81e1f2317d2a45c747b091bc76c57d2e20969
Yuval Shapira <uv1202@gmail.com>
2022-09-15 10:04:11 +0300

    a

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function App() {
+  function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </>
+    );
+  }
+  
+  function About() {
+    return (
+      <>
+        <main>
+          <h2>Who are we?</h2>
+          <p>
+            That feels like an existential question, don't you
+            think?
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to BattleShip</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </header>
+    </div>
+  );
+}
